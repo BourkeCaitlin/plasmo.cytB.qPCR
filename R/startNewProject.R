9bae58efedabad4e4032f1b1f872c513787f3814
BourkeCaitlin <bourke.c@wehi.edu.au>
2025-05-26 21:06:34 +1000

    add data folders too

--- R/startNewProject.R
+++ R/startNewProject.R
@@ -13,16 +13,25 @@ startNewProject <- function(project_name, species_type) {
   dir.create(here::here("results"),showWarnings = F)
   dir.create(here::here(paste0("results/", name)),showWarnings = F)
 
+  dir.create(here::here("data"),showWarnings = F)
+  dir.create(here::here(paste0("data/", name)),showWarnings = F)
+
   dir.create(here::here(paste0("results/", name, "/screening")),showWarnings = F)
+  dir.create(here::here(paste0("data/", name, "/screening")),showWarnings = F)
+
   dir.create(here::here(paste0("results/", name, "/screening/report")),showWarnings = F)
   dir.create(here::here(paste0("results/", name, "/screening/spreadsheet")),showWarnings = F)
 
   if (species_type=="nested") {
     dir.create(here::here(paste0("results/", name, "/nested_species")),showWarnings = F)
+    dir.create(here::here(paste0("data/", name, "/nested_species")),showWarnings = F)
+
     dir.create(here::here(paste0("results/", name, "/nested_species/report")),showWarnings = F)
     dir.create(here::here(paste0("results/", name, "/nested_species/spreadsheet")),showWarnings = F)
   }else if (species_type=="direct") {
       dir.create(here::here(paste0("results/", name, "/direct_species")),showWarnings = F)
+    dir.create(here::here(paste0("data/", name, "/direct_species")),showWarnings = F)
+
       dir.create(here::here(paste0("results/", name, "/direct_species/report")),showWarnings = F)
       dir.create(here::here(paste0("results/", name, "/direct_species/spreadsheet")),showWarnings = F)
     }
