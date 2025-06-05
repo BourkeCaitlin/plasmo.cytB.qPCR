36fc49aad194abb02c9c1cde886ebeb9f9a70017
BourkeCaitlin <bourke.c@wehi.edu.au>
2025-06-05 16:04:04 +1000

    change param input to _species to be consistent with usage in nested function

--- R/startNewProject.R
+++ R/startNewProject.R
@@ -1,14 +1,14 @@
 #' Generate folder structure to save results
 #'
 #' @param project project name to use in folder structure
-#' @param species_type either 'nested' or 'direct' depending on the type of species protocol to be use for the project
+#' @param species_type either 'nested_species' or 'direct_species' depending on the type of species protocol to be use for the project
 #' @param sub_directory if working within a folder at the root, specify this here. if not, leave blank
 #'
 #' @return folder structure at current working directory
 #' @export startNewProject
 #'
 #' @examples
-#' \dontrun{startNewProject(project = "example", sub_directory = "lab-molecular", species_type = "nested")}
+#' \dontrun{startNewProject(project = "example", sub_directory = "lab-molecular", species_type = "nested_species")}
 #'
 startNewProject <- function(project, sub_directory="", species_type) {
   dir.create(here::here(sub_directory), showWarnings = F)
@@ -22,14 +22,14 @@ startNewProject <- function(project, sub_directory="", species_type) {
   dir.create(here::here(paste0(sub_directory, "/", project, "/results/screening/report")), showWarnings = F)
   dir.create(here::here(paste0(sub_directory, "/", project, "/results/screening/spreadsheet")), showWarnings = F)
 
-  if (species_type=="nested") {
+  if (species_type=="nested_species") {
     dir.create(here::here(paste0(sub_directory,"/", project, "/data/nested_species")),showWarnings = F)
     dir.create(here::here(paste0(sub_directory,"/", project, "/results/nested_species")),showWarnings = F)
 
     dir.create(here::here(paste0(sub_directory,"/", project, "/results/nested_species/report")),showWarnings = F)
     dir.create(here::here(paste0(sub_directory,"/", project, "/results/nested_species/spreadsheet")),showWarnings = F)
 
-  }else if (species_type=="direct") {
+  }else if (species_type=="direct_species") {
     dir.create(here::here(paste0(sub_directory,"/", project, "/data/direct_species")),showWarnings = F)
     dir.create(here::here(paste0(sub_directory,"/", project, "/results/direct_species")),showWarnings = F)
 
