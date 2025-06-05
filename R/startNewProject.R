80d240883a0052d8b8b96242add321f36d94d6c3
BourkeCaitlin <bourke.c@wehi.edu.au>
2025-06-05 10:53:29 +1000

    add option for subdirectory in project setup

--- R/startNewProject.R
+++ R/startNewProject.R
@@ -2,6 +2,7 @@
 #'
 #' @param project project name to use in folder structure
 #' @param species_type either 'nested' or 'direct' depending on the type of species protocol to be use for the project
+#' @param sub_directory a subdirectory at the root (where RProj is) where working with molecular data
 #'
 #' @return folder structure at current working directory
 #' @export startNewProject
@@ -9,31 +10,31 @@
 #' @examples
 #' \dontrun{startNewProject("statem", "nested")}
 #'
-startNewProject <- function(project, species_type) {
-  dir.create(here::here("results"),showWarnings = F)
-  dir.create(here::here(paste0("results/", project)),showWarnings = F)
+startNewProject <- function(project, sub_directory="", species_type) {
+  dir.create(here::here(sub_directory), showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/")), showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/data/")), showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/results/")), showWarnings = F)
 
-  dir.create(here::here("data"),showWarnings = F)
-  dir.create(here::here(paste0("data/", project)),showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/results/screening/")), showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/data/screening/")), showWarnings = F)
 
-  dir.create(here::here(paste0("results/", project, "/screening")),showWarnings = F)
-  dir.create(here::here(paste0("data/", project, "/screening")),showWarnings = F)
-
-  dir.create(here::here(paste0("results/", project, "/screening/report")),showWarnings = F)
-  dir.create(here::here(paste0("results/", project, "/screening/spreadsheet")),showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/results/screening/report")), showWarnings = F)
+  dir.create(here::here(paste0(sub_directory, "/", project, "/results/screening/spreadsheet")), showWarnings = F)
 
   if (species_type=="nested") {
-    dir.create(here::here(paste0("results/", project, "/nested_species")),showWarnings = F)
-    dir.create(here::here(paste0("data/", project, "/nested_species")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/data/nested_species")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/results/nested_species")),showWarnings = F)
 
-    dir.create(here::here(paste0("results/", project, "/nested_species/report")),showWarnings = F)
-    dir.create(here::here(paste0("results/", project, "/nested_species/spreadsheet")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/results/nested_species/report")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/results/nested_species/spreadsheet")),showWarnings = F)
+
   }else if (species_type=="direct") {
-      dir.create(here::here(paste0("results/", project, "/direct_species")),showWarnings = F)
-    dir.create(here::here(paste0("data/", project, "/direct_species")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/data/direct_species")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/results/direct_species")),showWarnings = F)
 
-      dir.create(here::here(paste0("results/", project, "/direct_species/report")),showWarnings = F)
-      dir.create(here::here(paste0("results/", project, "/direct_species/spreadsheet")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/results/direct_species/report")),showWarnings = F)
+    dir.create(here::here(paste0(sub_directory,"/", project, "/results/direct_species/spreadsheet")),showWarnings = F)
     }
   else if(!species_type%in%c("direct", "nested")){
     print("please specify either 'nested' or 'direct' according to project plans")
